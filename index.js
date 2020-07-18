4a0b8a825c70d24afbb1a31b14b6014e4dd64c72
shadman-a <shadman@outlook.com>
2020-07-17 21:09:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-let formData = {
-    dogName: "Byron",
-    dogBreed: "Poodle"
-  };
   
-  // method: "POST" is missing from the object below
-  let configObj = {
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-  };
-
-fetch("http://localhost:3000/dogs", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle"
-  })
-});
+function submitData( userName, userEmail ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          userName,
+          userEmail
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
