1c2652315614ad8fa0c4f640ee04d3bb936ca08a
afrolambo <stephennoahgalvan@.com>
2020-07-19 11:33:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+let url = "http://localhost:3000/users"
+
+function submitData(name, email) {
+    return fetch (url, {
+        method: "POST", 
+        headers: {
+            "Content-type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            name, email
+        })
+        })  
+        .then( function ( response ) {
+            return response.json()
+          } )
+        .then(function (object) {
+            document.body.innerHTML = object[ "id" ]
+        } )
+        .catch(function(error) {
+            alert("Oopsie!");
+            document.body.innerHTML = error.message
+        });
+};
